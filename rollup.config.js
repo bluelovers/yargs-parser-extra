4bc92a3b8c199bca51f36cb1c07dc86d897ecc62
bluelovers <codelovers@users.sourceforge.net>
2020-01-19 18:15:18 +0800

    publish new version

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,57 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import minify from 'rollup-plugin-babel-minify';
+import typescript from '@rollup/plugin-typescript';
+import path from 'path';
 
-// rollup.config.js
-export default {
-	input: 'src/index.js',
-	output: {
-		file: 'dist/bundle.js',
-		format: 'cjs',
-		sourcemap: true,
-	},
+let extraOptions = {
+	external: [ 'tslib' ],
 	plugins: [
 		resolve({
-			preferBuiltins: true
+			preferBuiltins: true,
 		}),
-		commonjs(),
+		commonjs({
+			extensions: ['.js', '.ts', ],
+		}),
 		minify({
 			comments: false,
 		}),
 	],
 };
+
+// rollup.config.js
+export default [
+	{
+		input: 'src/index.js',
+		output: {
+			file: 'dist/bundle.js',
+			format: 'cjs',
+			sourcemap: true,
+		},
+		...extraOptions,
+		plugins: [
+			typescript({
+				module: 'CommonJS',
+				tsconfig: path.join(__dirname, 'tsconfig.json'),
+				inlineSourceMap: false,
+			}),
+			...extraOptions.plugins,
+		]
+	},
+//	{
+//		input: 'index.ts',
+//		output: {
+//			file: 'dist/bundle.es.js',
+//			format: 'es',
+//			sourcemap: true,
+//		},
+//		...extraOptions,
+//		plugins: [
+//			typescript({
+//				tsconfig: path.join(__dirname, 'tsconfig.json'),
+//				inlineSourceMap: false,
+//			}),
+//			...extraOptions.plugins,
+//		]
+//	},
+];
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const index = require("../index");
 
 module.exports = Object.assign(index.parseArgv, index);
+
+Object.defineProperty(module.exports, "__esModule", { value: true });
